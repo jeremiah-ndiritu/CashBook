b2345a389c158ecd23a2ba4c980ea805b82e5d0c
DigitalizedDev <digitalized.channel@gmail.com>
2025-10-08 13:14:50 +0300

    comment th

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import { initSW, enableSWRecovery } from "./utils/sw.js";
+// import { initSW, enableSWRecovery } from "./utils/sw.js";
 
-initSW();
-enableSWRecovery();
+// initSW();
+// enableSWRecovery();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
